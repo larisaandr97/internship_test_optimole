464653f5c80cc6d49061898d1405a877dae6223d
larisaandr97 <androniclarisa97@yahoo.com>
2020-12-26 22:28:37 +0200

    project complete

--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -2,20 +2,23 @@
 jest.setTimeout(900000);
 let { handler } = require("../index");
 test("Test your code", async done => {
-		try {
-			let response = await handler( { optimoleKey: 'bHVja3k=' });
-			expect(new Buffer(response.pass).toString('base64')).toBe('bHVja3k=');
-			expect(typeof response.optimized).toBe('object');
-			expect(response.optimized.length).toBe(7);
-			for (const optimized of response.optimized) {
-				expect(typeof optimized.filePath).toBe('string');
-				expect(typeof optimized.procent).toBe('number');
-				expect(optimized.procent < 100).toBeTruthy();
-				expect(optimized.procent > 50).toBeTruthy();
-			}
-			done();
+	try {
+		let response = await handler({ optimoleKey: 'bHVja3k=' });
+		expect(new Buffer(response.pass).toString('base64')).toBe('bHVja3k=');
+		expect(typeof response.optimized).toBe('object');
+		expect(response.optimized.length).toBe(7);
+		const regex = /^optimized\/[\w-\.]+\.([\w-])+[\w]$/;
+		for (const optimized of response.optimized) {
+			expect(typeof optimized.filePath).toBe('string');
+			expect(typeof optimized.procent).toBe('number');
+			expect(optimized.procent < 100).toBeTruthy();
+			//	expect(optimized.procent > 50).toBeTruthy();
+			// test if file name matches the expected path in directory 'optimized'
+			expect(optimized.filePath).toMatch(regex);
 		}
-		catch (err) {
-			done(err);
-		}
+		done();
+	}
+	catch (err) {
+		done(err);
+	}
 });
--- index.js
+++ index.js
@@ -1,4 +1,71 @@
-//here you can require you dependencies or external functions
-exports.handler = async function ( event ) {
-	//here you will add your code
+const sharp = require('sharp');
+const fs = require('fs');
+const sizeOf = require('image-size');
+
+exports.handler = async function (event) {
+
+	// creating directory with name 'optimized', if it doesn't exist yet
+	var dirName = './optimized';
+	if (!fs.existsSync(dirName)) {
+		fs.mkdirSync(dirName);
+	}
+
+	// decoding optimole key passed by event
+	let encodedKey = event['optimoleKey'];
+	let decodedKey = atob(encodedKey);
+
+	// create output object
+	var finalObject = new Object();
+	finalObject['pass'] = decodedKey;
+	finalObject['optimized'] = [];
+
+	// open directory where images are saved
+	const dir = fs.opendirSync('images')
+	let dirent
+	const files = [];
+
+	// storing file name from directory in array
+	while ((dirent = dir.readSync()) !== null) {
+		//console.log(dirent.name)
+		files.push(dirent.name);
+	}
+	dir.closeSync()
+
+	files.forEach(image => {
+
+		// creating object to add to final array
+		var obj = new Object();
+		obj['filePath'] = 'optimized/' + image;
+
+		// get original size of image
+		var dimensions = sizeOf('images/' + image);
+
+		// compute procentage of how much smaller the resulted image is
+		let area = dimensions.width * dimensions.height;
+		obj.procent = Math.round(
+			(Math.abs(area - 250000) / area) * 100);
+
+		finalObject.optimized.push(obj);
+
+		// resize image
+		sharp('images/' + image)
+			.resize({
+				fit: sharp.fit.contain,
+				width: 500,
+				height: 500
+			})
+			.flatten({ background: { r: 255, g: 255, b: 255 } })
+			.toBuffer()
+			.then(data => {
+				fs.writeFileSync('optimized/' + image, data);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+
+	})
+
+	console.log(finalObject);
+	return finalObject;
 };
+
